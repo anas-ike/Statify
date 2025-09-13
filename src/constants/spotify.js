a1f4d2cf27e10e55eafcfc3e476d2dff6847fe04
Lights.out.#0 <156470102+anas-ike@users.noreply.github.com>
2025-09-13 23:38:44 +0300

    Update spotify.js

--- src/constants/spotify.js
+++ src/constants/spotify.js
@@ -2,7 +2,7 @@ const MODE = import.meta.env.MODE;
 
 // Your actual URLs
 const DEV_URL = 'http://localhost:5173/';
-const PROD_URL = 'https://statify.lightsout.in/'; // ← your custom domain
+const PROD_URL = 'https://adorable-faun-ab9da1.netlify.app/'; // ← your custom domain
 
 const SCOPES = [
     'user-read-email',
