dfcd5db18511da82e238ab75b2e60dfc3c02de74
Lights.out.#0 <156470102+anas-ike@users.noreply.github.com>
2026-02-06 02:28:39 +0300

    Update CurrentUserContext to use null state
    Change initial state of currentUser to null and simplify provider return.

--- src/context/currentUser.jsx
+++ src/context/currentUser.jsx
@@ -1,18 +1,13 @@
 import { createContext, useState } from 'react'
 
-export const CurrentUserContext = createContext()
+export const CurrentUserContext = createContext(null)
 
 export function CurrentUserProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState([])
+  const [currentUser, setCurrentUser] = useState(null) // MUST be null, not []
 
-    return (
-        <CurrentUserContext.Provider
-            value={{
-                currentUser,
-                setCurrentUser
-            }}
-        >
-            {children}
-        </CurrentUserContext.Provider>
-    )
-}    
+  return (
+    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUserContext.Provider>
+  )
+}
