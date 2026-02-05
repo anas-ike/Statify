7651ef8fb00f02dbc1b5cb594d38e3994da95466
Lights.out.#0 <156470102+anas-ike@users.noreply.github.com>
2026-02-06 02:43:27 +0300

    Update currentUser.jsx

--- src/context/currentUser.jsx
+++ src/context/currentUser.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react'
 export const CurrentUserContext = createContext(null)
 
 export function CurrentUserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null) // MUST be null, not []
+  const [currentUser, setCurrentUser] = useState(null) // FIXED
 
   return (
     <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
