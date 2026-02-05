9eb67f8800f985449785421d6faac8c5013b8fa6
Lights.out.#0 <156470102+anas-ike@users.noreply.github.com>
2026-02-06 02:55:00 +0300

    Refactor accessUrl construction for better readability

--- src/api/getAuthorization.js
+++ src/api/getAuthorization.js
@@ -5,9 +5,8 @@ const { REDIRECT_URI, AUTH_ENDPOINT, SCOPES } = SPOTIFY;
 const RESPONSE_TYPE = 'token';
 
 export const accessUrl =
-    AUTH_ENDPOINT +
-    '?client_id=' + CLIENT_ID +
-    '&redirect_uri=' + REDIRECT_URI +
-    '&scope=' + SCOPES.join('%20') +
-    '&response_type=' + RESPONSE_TYPE + 
-    '&show_dialog=true';
+  `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}` +
+  `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+  `&scope=${encodeURIComponent(SCOPES.join(' '))}` +
+  `&response_type=${RESPONSE_TYPE}` +
+  `&show_dialog=true`;
