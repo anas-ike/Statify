4e6a48cb6b864e224b236fb000e3e167d056e119
Lights.out.#0 <156470102+anas-ike@users.noreply.github.com>
2026-02-06 14:18:41 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import { getAuthorization } from "../api/getAuthorization";
 
 export default function Login() {
+  function handleLogin() {
+    window.location.href = getAuthorization();
+  }
+
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <button
-        onClick={getAuthorization}
-        className="bg-green-500 px-6 py-3 rounded text-white font-bold"
+        onClick={handleLogin}
+        className="px-6 py-3 bg-green-500 text-white rounded-lg"
       >
         Login with Spotify
       </button>
